a426b59fe3f5c4e67f2e176ae589243e86b5fcf2
Savvy <sabrinarocker16@gmail.com>
2022-04-25 18:01:26 -0500

    adding code

---
+++ MultipleChoice.java
@@ -0,0 +1,8 @@
+public class MultipleChoice extends Question {
+    public String answer;
+
+    public MultipleChoice(String question, String answer) {
+        super(question);
+        this.answer = answer;
+    }
+}
---
+++ TrueOrFalse.java
@@ -0,0 +1,8 @@
+public class TrueOrFalse extends Question {
+    public String answer;
+
+    public TrueOrFalse(String question, String answer) {
+        super(question);
+        this.answer = answer;
+    }
+}
